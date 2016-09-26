b970ad97368c6839ef1902b3472e20eee0cd1030
Eder Trindade <eder.gabriel.felix@gmail.com>
2016-09-26 02:45:50 -0300

    .

--- assets/js/public/signup/controllers/SignupController.js
+++ assets/js/public/signup/controllers/SignupController.js
@@ -2,4 +2,10 @@ angular.module('SignupModule')
 
 .controller('SignupController', function ($scope) {
 	$scope.user = {};
+
+	$scope.checkPasswordMatch = function () {
+		if ($scope.user.password === $scope.user.confirmPassword) {
+			alert("UHUUUU")
+		}
+	}
 })
